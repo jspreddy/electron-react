e0f00aaaf5a7138ddb1d6c2f5e84ed0a5ca2bfac
Sai Phaninder Reddy Jonnala <sai.jonnala@gopassport.com>
2018-10-05 23:22:39 -0400

    WIP: prop type issues

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Dashboard from './Dashboard/Dashboard';
 
 export default class App extends React.Component {
+
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <Dashboard />
+    );
   }
 }
